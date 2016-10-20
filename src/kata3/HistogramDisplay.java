7479f909e064649805ee429cb6dfc2f9dd2f36ca
usuario <usuario@10.22.143.50>
2016-10-20 10:16:04 +0100

    Kata 3
    Comenzamos a utilizar los Paneles

---
+++ src/kata3/HistogramDisplay.java
@@ -0,0 +1,68 @@
+package kata3;
+
+import java.awt.Dimension;
+import javax.swing.JPanel;
+import org.jfree.chart.ChartFactory;
+import org.jfree.chart.ChartPanel;
+import org.jfree.chart.JFreeChart;
+import org.jfree.chart.plot.PlotOrientation;
+import org.jfree.data.category.DefaultCategoryDataset;
+import org.jfree.ui.ApplicationFrame;
+
+public class HistogramDisplay extends ApplicationFrame {
+
+    public HistogramDisplay() {
+        super(" Histogram");
+        
+        // establecemos panel contenedor
+        setContentPane(createPanel());
+        // pack() = pone la ventana con el tamaño mínimo para que todos
+        //los componentes sean visibles
+        pack();
+    }
+    
+    public void execute(){
+        // hacemos visible la ventana
+        setVisible(true);
+    }
+    
+    private JPanel createPanel(){
+        // creamos ventana, panel y panel para datos
+        ChartPanel chartPanel = new ChartPanel(createChart(createDataset()));
+        
+        
+        // establecemos tamaño
+        chartPanel.setPreferredSize(new Dimension(500,400));
+        
+        // devolvemos chartPanel
+        return chartPanel;
+    }
+    
+    
+    // creación del panel: titulo, etiqueta horizontal, etiqueta vertical, conjunto datos, orientación, etiqueta bajo gráfico, -, -.
+    private JFreeChart createChart(DefaultCategoryDataset dataSet){
+        JFreeChart chart = ChartFactory.createBarChart("JFreecharHistogram",
+                                                      "Dominois email", 
+                                                      "Nº Emails", 
+                                                      dataSet, 
+                                                      PlotOrientation.VERTICAL, 
+                                                      false, 
+                                                      rootPaneCheckingEnabled, 
+                                                      rootPaneCheckingEnabled 
+                );
+        return chart;
+        
+    }
+    
+    // creación del conjunto de datos
+    private DefaultCategoryDataset createDataset(){
+        DefaultCategoryDataset dataSet = new DefaultCategoryDataset();
+        dataSet.addValue(10, "", "gmail.com");
+        dataSet.addValue(15, "", "ulpgc.com");
+        dataSet.addValue(12, "", "ull.com");
+        dataSet.addValue(4, "", "hotmail.com");
+        
+        return dataSet;
+    }
+    
+}
